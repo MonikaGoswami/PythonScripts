82fb40bc148b407b3033857a1428042a81157062
Monika Goswami <monikagoswami1991@gmail.com>
2019-09-27 18:43:18 -0400

    Adding new file SeleniumTestCase.py

---
+++ MultiBarChart.py
@@ -0,0 +1,26 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+Ages_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+
+x_indexes=np.arange(len(Ages_x))
+width=0.25
+
+dev_y = [38496, 42000, 46752, 49320, 53200, 56000, 62316, 64928, 67317, 68748, 73752]
+plt.bar(x_indexes-width,dev_y,color='k',width=width,label='All Developers')
+
+py_dev_y = [45372, 48876, 53850, 57287, 63016, 65998, 70003, 70000, 71496, 75370, 83640]
+plt.bar(x_indexes,py_dev_y,color='b',width=width,label='Python Developers')
+
+js_dev_y = [37810, 43515, 46823, 49293, 53437, 56373, 62375, 66674, 68745, 68746, 74583]
+plt.bar(x_indexes+width,js_dev_y,color='g',width=width,label='JS Developers')
+
+plt.xticks(ticks=x_indexes,labels=Ages_x)
+
+plt.legend()
+
+plt.title('Salary median for Dev in USD')
+plt.xlabel('Developer Ages')
+plt.ylabel('Developer Salary')
+
+plt.show()
